6bb0f658d5cc584327a7065fe781b8109ceeba7a
Semen <49660403+semen-bogdanov@users.noreply.github.com>
2024-10-12 14:06:15 +0300

    Update verification-user.tsx

--- shared/components/shared/email-temapltes/verification-user.tsx
+++ shared/components/shared/email-temapltes/verification-user.tsx
@@ -11,7 +11,7 @@ export const VerificationUserTemplate: React.FC<Props> = ({ code }) => (
     </p>
 
     <p>
-      <a href={`http://localhost:3000/api/auth/verify?code=${code}`}>Подтвердить регистрацию</a>
+      <a href={`https://nextjspizza-git-main-semenbogdanovs-projects.vercel.app/verify?code=${code}`}>Подтвердить регистрацию</a>
     </p>
   </div>
 );
